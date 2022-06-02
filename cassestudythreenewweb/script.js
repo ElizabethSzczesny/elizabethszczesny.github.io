54f1469015bc1256a4910185b6b1654e53b71b46
Elizabeth Szczesny <elizabeth.szczesny@gmail.com>
2022-06-02 14:11:27 -0400

    Portfolio

---
+++ cassestudythreenewweb/script.js
@@ -0,0 +1,35 @@
+/*var images = document.getElementsByClassName('box-shadow');
+var forward = document.getElementById('forward');
+var backward = document.getElementById('backward');
+
+forward.addEventListener('click', function(e){
+
+    console.log(e.currentTarget.className);
+    console.log(e);
+
+    for (let i=0; i < images.length; i++){
+        if (images[i].className === i){
+            dots[i].classList.add('active'); 
+            images[i].style.display = 'block';
+        }
+        if (dots[i].className !== e.currentTarget.className){
+            dots[i].classList.remove('active');
+            images[i].style.display = 'none';
+        } 
+    }
+
+});*/
+
+/*let slider = tns({
+    container: ".slideshow-image-wrapper",
+    "slideBy": 1,
+    "speed": 600,
+    "nav": false,
+    gutter: 100,
+    center:true,
+    controlsContainer: ".side-by-side",
+    prevButton:"#backward",
+    nextButton:"#forward",
+})*/
+
+
